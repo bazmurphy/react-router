aec069212789e59f4ade0f292547b0d34d2641b2
Baz Murphy <bazmurphy@gmail.com>
2023-06-19 10:48:31 +0100

    02-basics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+// Browser Router is a component that wraps the entire application, allows us to use the Router within it
+// Routes Components - is a parent component for the routes
+// Route Component - takes 2 props, path for the route, and the element to render
+// Link Component - Link between different pages - props: path element
+// Nav Link - it can add Active Classes which recognises which page you are on and add
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>Job Router</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,31 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sit odio
+        minima voluptatibus. Quis sint mollitia ex, ipsa nam labore totam
+        doloribus distinctio repellendus assumenda recusandae delectus magni
+        minima adipisci hic doloremque cupiditate, sequi similique excepturi
+        iste. Porro cupiditate culpa asperiores, nobis delectus nesciunt
+        repudiandae maxime est nam? Quos, ipsam!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sit odio
+        minima voluptatibus. Quis sint mollitia ex, ipsa nam labore totam
+        doloribus distinctio repellendus assumenda recusandae delectus magni
+        minima adipisci hic doloremque cupiditate, sequi similique excepturi
+        iste. Porro cupiditate culpa asperiores, nobis delectus nesciunt
+        repudiandae maxime est nam? Quos, ipsam!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sit odio
+        minima voluptatibus. Quis sint mollitia ex, ipsa nam labore totam
+        doloribus distinctio repellendus assumenda recusandae delectus magni
+        minima adipisci hic doloremque cupiditate, sequi similique excepturi
+        iste. Porro cupiditate culpa asperiores, nobis delectus nesciunt
+        repudiandae maxime est nam? Quos, ipsam!
+      </p>
+    </div>
+  );
+}
